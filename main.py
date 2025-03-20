4d81db3b15aa3ae6c33486be43af17ba5b4a5754
Adam Fišer <adam.fiser@wanex.cz>
2025-03-20 08:12:40 +0100

    Nová funkce mocnina
    #14

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     y = 10
     vysledek = secti(x, y)
     print(f"Výsledek sčítání {x} a {y} je: {vysledek}")
+    print(f"Výsledek mocniny {x} na {y} je: {mocnina(x,y)}")
 
 if __name__ == '__main__':
     main()
