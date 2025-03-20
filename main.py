40a7714a37bbce576fc97c2f46c47d36464ba000
Jan Zapletal <d23828oa-opava.cz>
2025-03-20 08:13:16 +0100

    Nová funkce obvod obdélníku

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from utils import secti
+from utils import *
 
 def main():
     x = 5
     y = 10
     vysledek = secti(x, y)
     print(f"Výsledek sčítání {x} a {y} je: {vysledek}")
+    vysledek2 = obvod_obdelniku(5,7)
+    print(f"Obvod obdélníku je:", vysledek2)
 
 if __name__ == '__main__':
     main()
